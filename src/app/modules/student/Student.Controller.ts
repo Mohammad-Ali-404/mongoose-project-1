2b4c39d727f0cb6898fcddac9b3ce03c79050ceb
wpmohammad1 <alimohammadjhd@gmail.com>
2024-06-06 14:29:00 +0600

    catch async

--- src/app/modules/student/Student.Controller.ts
+++ src/app/modules/student/Student.Controller.ts
@@ -1,46 +1,36 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { StudentServices } from "./student.service";
-import studentSchema from "./student.validation";
 import { z } from "zod";
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
-    // creating a schema validation using joi
-
-    const { student: studentData } = req.body;
-    const studentValidationSchema = z.object({
-      id: z.string(),
-      name: z.object({
-        firstName: z.string(),
-        middleName: z.string(),
-        lastName: z.string(),
-      }),
-    });
-    // const { value, error } = studentSchema.validate(studentData);
-    // console.log(error, value);
-  } catch (error) {
-    console.log(error);
-  }
+const catchAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+  };
 };
 
-const getAllStudents = async (req: Request, res: Response) => {
-  try {
-    const result = await StudentServices.getAllStudentFromDB();
-    res.status(200).json({
-      success: true,
-      message: "Students are retrieved successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-const getSingleStudents = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
+const createStudent = catchAsync(async (req, res, next) => {
+  // creating a schema validation using joi
+  const { student: studentData } = req.body;
+  const studentValidationSchema = z.object({
+    id: z.string(),
+    name: z.object({
+      firstName: z.string(),
+      middleName: z.string(),
+      lastName: z.string(),
+    }),
+  });
+});
+
+const getAllStudents = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentServices.getAllStudentFromDB();
+  res.status(200).json({
+    success: true,
+    message: "Students are retrieved successfully",
+    data: result,
+  });
+});
+const getSingleStudents = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { studentId } = req.params;
     const result = await StudentServices.getSingleStudentFromDB(studentId);
     res.status(200).json({
@@ -48,10 +38,8 @@ const getSingleStudents = async (
       message: "Students is retrieved successfully",
       data: result,
     });
-  } catch (error) {
-    console.log(error);
   }
-};
+);
 export const studentController = {
   getAllStudents,
   getSingleStudents,
