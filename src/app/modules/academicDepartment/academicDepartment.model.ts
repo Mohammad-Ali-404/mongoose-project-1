5eb9a9d69a123870065f92184cdb909442d2fa54
wpmohammad1 <alimohammadjhd@gmail.com>
2024-06-14 03:11:35 +0600

    model file

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -17,6 +17,25 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     timestamps: true,
   }
 );
+
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExists = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExists) {
+    throw new Error("Department is Already exists");
+  }
+  next();
+});
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExists = await AcademicDepartment.findOne(query);
+  if (!isDepartmentExists) {
+    throw new Error("this Department does not exists");
+  }
+  next();
+});
 export const AcademicDepartment = model<TAcademicDepartment>(
   "AcademicDepartment",
   academicDepartmentSchema
