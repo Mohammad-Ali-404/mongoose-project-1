19c509087a75fa92f15f787225f561dde31c9a46
wpmohammad1 <alimohammadjhd@gmail.com>
2024-06-16 16:35:49 +0600

    Filtering ,Sorting And Limiting

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -2,16 +2,23 @@ import mongoose from "mongoose";
 import { Student } from "./student.model";
 
 const getAllStudentFromDB = async (query: Record<string, unknown>) => {
+  const queryObj = { ...query };
+
   let searchTerm = "";
   if (query?.searchTerm) {
     searchTerm = query?.searchTerm as string;
   }
-
-  const result = await Student.find({
+  const searchQuery = Student.find({
     $or: ["email", "name.firstName", "presentAddress"].map((field) => ({
       [field]: { $regex: searchTerm, $options: "i" },
     })),
-  })
+  });
+
+  // filtering
+  const excludeFields = ["searchTerm", "sort", "limit"];
+  excludeFields.forEach((el) => delete queryObj[el]);
+  const filterQuery = await searchQuery
+    .find(queryObj)
     // .populate("admissionSemister")
     .populate({
       path: "academicDepartment",
@@ -19,7 +26,22 @@ const getAllStudentFromDB = async (query: Record<string, unknown>) => {
         path: "academicFaculty",
       },
     });
-  return result;
+
+  let sort = "-createAt";
+
+  if (query.sort) {
+    sort = query.sort as string;
+  }
+
+  const sortQuery = await filterQuery.sort();
+
+  let limit = 1;
+  if (query.limit) {
+    limit = query.limit;
+  }
+
+  const limitQuery = await sortQuery.limit(limit);
+  return limitQuery;
 };
 const getSingleStudentFromDB = async (id: string) => {
   const result = await Student.findById(id).populate({
